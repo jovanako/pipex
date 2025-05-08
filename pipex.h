cf9038c6207e84dbb832acdb589e477b798a12f3
Jovana Kovacevic <jovana1005@gmail.com>
2025-05-08 21:07:17 +0200

    deleted struct definition

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: jkovacev <jkovacev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 20:12:21 by jkovacev          #+#    #+#             */
-/*   Updated: 2025/05/08 19:51:59 by jkovacev         ###   ########.fr       */
+/*   Updated: 2025/05/08 21:06:00 by jkovacev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,3 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/wait.h>
-
-typedef struct  s_list
-{
-    int     input_fd;
-    int     output_fd;
-}   t_list;
