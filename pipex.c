fbf7652c383fac9d88942d67e3c080c7dc3840e4
Jovana Kovacevic <jovana1005@gmail.com>
2025-05-08 21:05:39 +0200

    made two fork functions

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: jkovacev <jkovacev@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 20:12:12 by jkovacev          #+#    #+#             */
-/*   Updated: 2025/05/08 20:50:47 by jkovacev         ###   ########.fr       */
+/*   Updated: 2025/05/08 21:04:58 by jkovacev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int perror_and_return(char *s, int n)
     return (n);
 }
 
-int fork_child(char *cmd, int input_fd, int output_fd, char *envp[], int to_close)
+int fork_pipe_left(char *cmd, int input_fd, int fd[], char *envp[])
 {
     pid_t   pid;
     char    *args[] = { cmd, NULL };
@@ -28,13 +28,36 @@ int fork_child(char *cmd, int input_fd, int output_fd, char *envp[], int to_clos
         perror_and_return("Error: Fork failed", 0);
     if (pid == 0)
     {
-        if (dup2(output_fd, 1) == -1)
+        if (dup2(fd[1], 1) == -1)
             perror_and_return("Error: dup failed", 0);
         if (dup2(input_fd, 0) == -1)
             perror_and_return("Error: dup failed", 0);
         close(input_fd);
+        close(fd[0]);
+        close(fd[1]);
+        execve(cmd, args, envp);
+        perror_and_return("Error: execve failed", 0);
+    }
+    return (1);
+}
+
+int fork_pipe_right(char *cmd, int output_fd, int fd[], char *envp[])
+{
+    pid_t   pid;
+    char    *args[] = { cmd, NULL };
+
+    pid = fork();
+    if (pid == -1)
+        perror_and_return("Error: Fork failed", 0);
+    if (pid == 0)
+    {
+        if (dup2(fd[0], 0) == -1)
+            perror_and_return("Error: dup failed", 0);
+        if (dup2(output_fd, 1) == -1)
+            perror_and_return("Error: dup failed", 0);
         close(output_fd);
-        close(to_close);
+        close(fd[0]);
+        close(fd[1]);
         execve(cmd, args, envp);
         perror_and_return("Error: execve failed", 0);
     }
@@ -54,11 +77,11 @@ int main(int argc, char *argv[], char *envp[])
     input_fd = open(argv[1], O_RDONLY);
     if (input_fd == -1)
         perror_and_return("Error: Open failed", 1);
-    fork_child(argv[2], input_fd, fd[1], envp, fd[0]);       
+    fork_pipe_left(argv[2], input_fd, fd, envp);       
     output_fd = open(argv[4], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (output_fd == -1)
         perror_and_return("Error: Open failed", 1);
-    fork_child(argv[3], fd[0], output_fd, envp, fd[1]);
+    fork_pipe_right(argv[3], output_fd, fd, envp);
     close(fd[0]);
     close(fd[1]);
     wait(NULL);
